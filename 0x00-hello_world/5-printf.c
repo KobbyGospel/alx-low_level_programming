07372c2c7bbfeaee670fc99f22b85bb5ef71fbdb
KobbyGospel <amososeibaah@gmail.com>
2023-02-20 06:43:44 -0800

    display text using printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+  * main - This function prints out a text as an output
+  * using the printf function
+  * Return: The return is 0 for success
+  */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
