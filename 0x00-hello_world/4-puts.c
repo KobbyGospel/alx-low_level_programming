d29c58f5ecdcb78e38e539608e63e4f6deaa3eb7
KobbyGospel <amososeibaah@gmail.com>
2023-02-20 05:49:14 -0800

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-/*
- * 4-puts.c: Programming is like building a multilingual puzzle
+/**
+ * main - This function displays a stringed text using puts function
+ * Return: This returns 0 for success
  */
 #include <stdio.h>
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
