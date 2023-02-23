146223a5e4c66b592b4ea8ca1802ebcb70caec8b
KobbyGospel <amososeibaah@gmail.com>
2023-02-23 04:50:40 -0800

    print textline using write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-/**
- * main - This program displays a text to the stdout
- * Retun: THe function returns 1 for success
- */
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main - This program prints a text line to the stdout
+ * Return: The program returns 1 for success
+ */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 50);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
