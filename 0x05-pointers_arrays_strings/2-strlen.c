5c2bfe0096431939f712227e5c89449491347f53
KobbyGospel <amososeibaah@gmail.com>
2023-03-01 05:42:16 +0000

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: the sting to be checked
+ *
+ * Return: an int value for len of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
